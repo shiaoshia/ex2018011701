043c0c3a15d4ba487039a1e0bdcd87f978cd5f23
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-17 14:10:57 +0800

    新增資料ok

--- app/src/main/java/tw/com/shiaoshia/ex2018011701/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018011701/MainActivity.java
@@ -5,15 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import tw.com.shiaoshia.ex2018011701.data.Student;
 import tw.com.shiaoshia.ex2018011701.data.StudentScoreDAO;
 
 public class MainActivity extends AppCompatActivity {
     final public static StudentScoreDAO dao = new StudentScoreDAO();
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        lv = (ListView)findViewById(R.id.listView);
+        ArrayList<String> studentName = new ArrayList<>();
+        for(Student s : dao.mylist) {
+            studentName.add(s.name);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,studentName);
+        lv.setAdapter(adapter);
     }
 
     @Override
