ac585a7f00691557b1858507b6fa1240099d547c
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-18 14:37:54 +0800

    建立StudentDAO interface StudentFileDAO.java與StudentScoreDAO要implements它

--- app/src/main/java/tw/com/shiaoshia/ex2018011701/data/StudentScoreDAO.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018011701/data/StudentScoreDAO.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Created by USER-NB on 2018/1/17.
  */
 
-public class StudentScoreDAO {
+public class StudentScoreDAO implements StudentDAO{
     public ArrayList<Student> mylist;
     public StudentScoreDAO() {
         mylist = new ArrayList<>();
